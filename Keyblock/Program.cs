92fb668add09f9b78b3cf49fe89215f0a566dd9f
Jan Saris <jan.saris@gmail.com>
2016-02-22 19:29:06 +0100

    Implemented get session key

--- Keyblock/Program.cs
+++ Keyblock/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +7,145 @@ namespace Keyblock
 {
     class Program
     {
+        private int DEBUG = 1;
+        private readonly Random _random = new Random();
+        private readonly SslTcpClient _sslClient = new SslTcpClient();
+        private string _apiMachineId;
+        private string _apiClientId;
+
+        // Cert data
+        const string szAddress = "6650 Lusk Blvd, Suite B203";
+        const string szZipCode = "92021";
+        const string szCountry = "US";
+        const string szProvince = "CA";
+        const string szCity = "San Diego";
+        const string szOrganization = "vr2.3.1-candidate-amino-A130.11-hwonly";
+        const string szCommon = "STB";
+        const string szTelephone = "858-677-7800";
+        const string szChallengePassword = "VODPassword";
+
+        // Connection data
+        string vcasServerAddress = string.Empty;         // Your VCAS server address
+        string vksServerAddress = string.Empty;          // Your VCAS server address
+        int VCAS_Port_SSL;              // Your VCAS port
+        int VKS_Port_SSL;               // Your VKS port
+
+        // API data
+        string api_company;               // Your company
+        const string api_msgformat = "1154"; 		// Only 1154 is supported for now
+
+        private string session_key;
+        private string timestamp;
+
         static void Main(string[] args)
         {
+            var prog = new Program();
+            prog.load_clientId();
+            prog.load_machineid();
+            prog.vm_config(
+                "vmx.stb.zt6.nl",
+                12697,
+                "vmx.stb.zt6.nl",
+                12699,
+                "Reggefiber",
+                1,
+                "dir");
+            if (!prog.API_GetSessionKey())
+            {
+                Console.WriteLine("Failed to get the session key");
+            }
+            Console.WriteLine("Give the server some time to process");
+            Task.Delay(1000).Wait();
+            prog.API_GetCertificate();
+            Console.WriteLine("Done: Exit");
+            Console.ReadKey();
+        }
+
+        private void API_GetCertificate()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool API_GetSessionKey()
+        {
+            var msg = $"{api_msgformat}~{_apiClientId}~CreateSessionKey~{api_company}~{_apiMachineId}~";
+
+            LOG(DEBUG, $"[API] Requesting Session Key: {msg}");
+            var resp = _sslClient.ssl_client_send(msg, vcasServerAddress, VCAS_Port_SSL);
+
+            if (resp == null) return false;
+
+            var encoding = Encoding.ASCII;
+
+            var responseBuffer = encoding.GetChars(resp);
+            session_key = new string(responseBuffer, 4, 16);
+            timestamp = new string(responseBuffer, 20, 20);
+            LOG(DEBUG, $"[API] Session key '{session_key}' obtained, timestamp: '{timestamp}' with encoding {encoding.EncodingName}");
+            
+            return true;
+        }
+
+        void vm_config(string vcas_address, int vcas_port, string vks_address, int vks_port, string company, int interval, string dir)
+        {
+            vcasServerAddress = vcas_address;
+            vksServerAddress = vks_address;
+
+            api_company = company;
+
+            VCAS_Port_SSL= vcas_port;
+            VKS_Port_SSL = vks_port;
+
+
+        }
+
+        int load_machineid()
+        {
+            _apiMachineId = string.Empty;
+            
+            if (File.Exists("machineId"))
+            {
+                LOG(DEBUG, "[API] MachineID found, reading MachineID");
+                _apiMachineId = File.ReadAllText("machineId");
+                if (_apiMachineId.Length == 28) return 0;
+            }
+            
+            LOG(DEBUG, "[API] No MachineID found, generating MachineID");
+            var buf = new byte[20];
+            _random.NextBytes(buf);
+            _apiMachineId = Convert.ToBase64String(buf);
+            LOG(DEBUG, $"[API] Your MachineID is: {_apiMachineId}");
+            File.WriteAllText("machineId",_apiMachineId);
+            return 0;
+        }
+
+        int load_clientId()
+        {
+            _apiClientId = string.Empty;
+
+            if (File.Exists("clientId"))
+            {
+                LOG(DEBUG, "[API] ClientID found, reading ClientId");
+                _apiClientId = File.ReadAllText("clientId");
+                if (_apiClientId.Length == 56) return 0;
+            }
+
+            LOG(DEBUG, "[API] No ClientId found, generating ClientId");
+            var buf = new byte[28];
+            _random.NextBytes(buf);
+            _apiClientId = string.Empty;
+            foreach (var b in buf)
+            {
+                _apiClientId += (b & 0xFF).ToString("X2");
+            }
+            //_apiClientId = Convert.ToBase64String(buf);
+            LOG(DEBUG, $"[API] Your ClientID is: {_apiClientId}");
+            File.WriteAllText("clientId", _apiClientId);
+            return 0;
+        }
+
+        private void LOG(int level, string message)
+        {
+            Console.WriteLine(message);
         }
     }
 }
---
+++ Keyblock/SslTcpClient.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Keyblock
+{
+    public class SslTcpClient
+    {
+        private static Hashtable certificateErrors = new Hashtable();
+
+        // The following method is invoked by the RemoteCertificateValidationDelegate.
+        public bool ValidateServerCertificate(
+              object sender,
+              X509Certificate certificate,
+              X509Chain chain,
+              SslPolicyErrors sslPolicyErrors)
+        {
+            if (sslPolicyErrors == SslPolicyErrors.None)
+                return true;
+
+            Console.WriteLine("Certificate error: {0}", sslPolicyErrors);
+
+            // Do not allow this client to communicate with unauthenticated servers.
+            return true;
+        }
+
+        public byte[] ssl_client_send(string msg, string server, int port)
+        {
+            // Create a TCP/IP client socket.
+            // machineName is the host running the server application.
+            TcpClient client = new TcpClient(server, port);
+            Console.WriteLine("Client connected.");
+            // Create an SSL stream that will close the client's stream.
+            using (SslStream sslStream = new SslStream(client.GetStream(), true, ValidateServerCertificate, null))
+            {
+                // The server name must match the name on the server certificate.
+                try
+                {
+                    sslStream.AuthenticateAsClient(server);
+
+                    var cert = sslStream.RemoteCertificate;
+                    Console.WriteLine($"Got certificate from server {cert.Issuer} with subject {cert.Subject}");
+                }
+                catch (AuthenticationException e)
+                {
+                    Console.WriteLine("Exception: {0}", e.Message);
+                    if (e.InnerException != null)
+                    {
+                        Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                    }
+                    Console.WriteLine("Authentication failed - closing the connection.");
+                    client.Close();
+                    return null;
+                }
+                // Encode a test message into a byte array.
+                // Signal the end of the message using the "<EOF>".
+                byte[] messsage = Encoding.ASCII.GetBytes(msg);
+                // Send hello message to the server. 
+                sslStream.Write(messsage);
+                sslStream.Flush();
+                // Read message from the server.
+                var serverMessage = ReadMessage(sslStream);
+
+                // Close the client connection.
+                client.Close();
+                Console.WriteLine("Client closed.");
+                return serverMessage;
+            }
+        }
+        
+        byte[] ReadMessage(SslStream sslStream)
+        {
+            // Read the  message sent by the server.
+            // The end of the message is signaled using the
+            // "<EOF>" marker.
+            var buffer = new byte[2048];
+            var receivedBytes = new List<byte>();
+            int bytes;
+            do
+            {
+                bytes = sslStream.Read(buffer, 0, buffer.Length);
+                receivedBytes.AddRange(buffer.Take(bytes));
+
+                //// Use Decoder class to convert from bytes to UTF8
+                //// in case a character spans two buffers.
+                //Decoder decoder = Encoding.ASCII.GetDecoder();
+                //char[] chars = new char[decoder.GetCharCount(buffer, 0, bytes)];
+                //decoder.GetChars(buffer, 0, bytes, chars, 0);
+                //messageData.Append(chars);
+                // Check for EOF.
+                //if (messageData.ToString().IndexOf("<EOF>") != -1)
+                //{
+                //    break;
+                //}
+            } while (bytes != 0);
+
+            return receivedBytes.ToArray();
+        }
+    }
+}
