c520b28189d3ca4488ba61dbfb95b0599e8f98ce
Jan Saris <jan.saris@gmail.com>
2016-02-24 18:56:50 +0100

    Added settings and logging

---
+++ Keyblock/IniSettings.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using log4net;
+
+namespace Keyblock
+{
+    public class IniSettings
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(IniSettings));
+
+        public string MachineId { get; set; }
+        public string ClientId { get; set; }
+
+        // Cert data
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
+        public string Province { get; set; }
+        public string City { get; set; }
+        public string Organization { get; set; }
+        public string Common { get; set; }
+        public string Telephone { get; set; }
+        public string Email { get; set; }
+        public string EmailHost { get; set; }
+        public string ChallengePassword { get; set; }
+
+        // Connection data
+        public string VcasServer { get; set; }
+        public string VksServer { get; set; }
+        public int VcasPort { get; set; }
+        public int VksPort { get; set; }
+
+        // API data
+        public string Company { get; set; }
+        public string MessageFormat { get; set; }
+
+        public void Load()
+        {
+            Logger.Info("Read Keyblock.ini");
+            try
+            {
+                using (var reader = new StreamReader("Keyblock.ini"))
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        if (string.IsNullOrWhiteSpace(line)) continue;
+                        if (line.StartsWith("#")) continue;
+                        Logger.DebugFormat("Process line {0}", line);
+                        ReadConfigItem(line);
+                    }
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to load the configuration", ex);
+            }
+        }
+
+        private void ReadConfigItem(string line)
+        {
+            var keyvalue = line.Split('=');
+            if (keyvalue.Length < 2)
+            {
+                Logger.WarnFormat("Failed to read configuration line: {0}", line);
+                return;
+            }
+            SetValue(keyvalue[0], keyvalue[1]);
+        }
+
+        private void SetValue(string key, string value)
+        {
+            var propertyInfo = GetType().GetProperty(key, BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.IgnoreCase | BindingFlags.Public);
+            if (propertyInfo == null)
+            {
+                Logger.WarnFormat("Unknown configuration key: {0}", key);
+                return;
+            }
+            try
+            {
+                Logger.DebugFormat("Read configuration item {0} with value {1}", key, value);
+                propertyInfo.SetValue(this, Convert.ChangeType(value, propertyInfo.PropertyType), null);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error($"Failed to read {key} into {value} as {propertyInfo.PropertyType}", ex);
+            }
+        }
+    }
+}
--- Keyblock/Program.cs
+++ Keyblock/Program.cs
@@ -2,71 +2,48 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-using System.Threading.Tasks;
-using Org.BouncyCastle.Asn1;
+using log4net;
+using log4net.Config;
 using Org.BouncyCastle.Asn1.X509;
 
 namespace Keyblock
 {
     class Program
     {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(Program));
+
         private bool noConnect = false;
 
-        private int DEBUG = 1;
         private readonly Random _random = new Random();
+        private readonly IniSettings _settings = new IniSettings();
         private readonly SslTcpClient _sslClient = new SslTcpClient();
-        private string _apiMachineId;
-        private string _apiClientId;
 
-        // Cert data
-        const string szAddress = "6650 Lusk Blvd, Suite B203";
-        const string szZipCode = "92021";
-        const string szCountry = "US";
-        const string szProvince = "CA";
-        const string szCity = "San Diego";
-        const string szOrganization = "vr2.3.1-candidate-amino-A130.11-hwonly";
-        const string szCommon = "STB";
-        const string szTelephone = "858-677-7800";
-        string szEmail = "VODPassword";
-        const string szChallengePassword = "VODPassword";
+        private string _sessionKey;
+        private string _timestamp;
 
-        // Connection data
-        string vcasServerAddress = string.Empty;         // Your VCAS server address
-        string vksServerAddress = string.Empty;          // Your VCAS server address
-        int VCAS_Port_SSL;              // Your VCAS port
-        int VKS_Port_SSL;               // Your VKS port
-
-        // API data
-        string api_company;               // Your company
-        const string api_msgformat = "1154"; 		// Only 1154 is supported for now
-
-        private string session_key;
-        private string timestamp;
-
-        static void Main(string[] args)
+        static void Main()
         {
+            XmlConfigurator.Configure(new FileInfo("Log4net.config"));
             var prog = new Program();
+            prog.LoadIni();
             prog.load_clientId();
             prog.load_machineid();
-            prog.vm_config(
-                "vmx.stb.zt6.nl",
-                12697,
-                "vmx.stb.zt6.nl",
-                12699,
-                "Reggefiber",
-                1,
-                "dir");
             if (!prog.API_GetSessionKey())
             {
                 Console.WriteLine("Failed to get the session key");
                 return;
             }
-            Console.WriteLine("Give the server some time to process");
+            Logger.Info("Give the server some time to process");
             //Task.Delay(1000).Wait();
             prog.API_GetCertificate();
             prog.Close();
-            Console.WriteLine("Done: Exit");
+            Logger.Info("Done: Exit");
             Console.ReadKey();
+        }
+
+        private void LoadIni()
+        {
+            _settings.Load();
         }
 
         private void Close()
@@ -74,158 +51,115 @@ namespace Keyblock
             _sslClient.Close();
         }
 
-        private bool API_GetCertificate()
+        private void API_GetCertificate()
         {
-            //FILE* fp;
-            //const uchar* cert;
-            //char* csr;
-            //int response_len;
-            //int msglen;
-            //uchar msg[2048];
-            //uchar* response_buffer = calloc(2048, 1);
             /******* Get the current time64 *******/
             var t64 = (long)(DateTime.UtcNow - new DateTime (1970, 1, 1)).TotalSeconds;
 
             /******* Generate the CSR *******/
-            LOG(DEBUG, "[API] Generating CSR");
-            //szEmail = calloc(64, 1);
-            szEmail = $"{_apiMachineId}.{t64}@Verimatrix.com";
-            //sprintf(szEmail, "%s.%llu@Verimatrix.com", api_machineId, t64);
-            LOG(DEBUG, $"[API] Using email: {szEmail}");
+            Logger.Debug("[API] Generating CSR");
+            _settings.Email = $"{_settings.MachineId}.{t64}@{_settings.EmailHost}";
+            Logger.Debug($"[API] Using email: {_settings.Email}");
             var csr = generate_csr();
             var pem = csr.CsrPem();
 
             /******* Generate the request string *******/
-            var msg = $"{api_msgformat}~{_apiClientId}~getCertificate~{api_company}~NA~NA~{pem}~{szCommon}~{szAddress}~ ~{szCity}~{szProvince}~{szZipCode}~{szCountry}~{szTelephone}~{szEmail}~{_apiMachineId}~{szChallengePassword}~";
-            //msglen = sprintf((char*)msg,
-            //                "%s~%s~getCertificate~%s~NA~NA~%s~%s~%s~ ~%s~%s~%s~%s~%s~%s~%s~%s~",
-            //                api_msgformat, api_clientID, api_company, csr,
-            //                szCommon, szAddress, szCity, szProvince, szZipCode, szCountry, szTelephone, szEmail,
-            //                api_machineID, szChallengePassword);
-            File.WriteAllText("winCert.txt", msg);
-            File.WriteAllText("cert.der", pem);
-            File.WriteAllText("csr.cer",$"-----BEGIN CERTIFICATE REQUEST-----{csr.Csr}-----END CERTIFICATE REQUEST-----");
-            LOG(DEBUG, $"[API] Requesting Certificate: {msg}");
+            var msg = $"{_settings.MessageFormat}~{_settings.ClientId}~getCertificate~{_settings.Company}~NA~NA~{pem}~{_settings.Common}~{_settings.Address}~ ~{_settings.City}~{_settings.Province}~{_settings.ZipCode}~{_settings.Country}~{_settings.Telephone}~{_settings.Email}~{_settings.MachineId}~{_settings.ChallengePassword}~";
+
+            Logger.Debug($"[API] Requesting Certificate: {msg}");
 
             /******* Send the request *******/
-            var response = _sslClient.Send(msg, vcasServerAddress, VCAS_Port_SSL);
+            var response = _sslClient.Send(msg, _settings.VcasServer, _settings.VcasPort);
 
             if (response == null || response.Length < 12)
             {
-                return false;
+                return;
             }
 
             /******* Get the Signed cert from the response *******/
             var cert = new List<byte>(response).GetRange(12, (response.Length - 12)).ToArray();
             File.WriteAllBytes("signed_cert.cer",cert);
-            //cert = response_buffer + 12;
-            
-            return true;
         }
 
         private X509CertificateRequest generate_csr()
         {
             var subject = new X509SubjectAttributes();
-            subject.Add(X509Name.C,szCountry);
-            subject.Add(X509Name.ST, szProvince);
-            subject.Add(X509Name.L, szCity);
-            subject.Add(X509Name.O, api_company);
-            subject.Add(X509Name.OU, szOrganization);
-            subject.Add(X509Name.CN,szCommon);
-            subject.Add(X509Name.EmailAddress,szEmail);
-            subject.ChallangePassword(szChallengePassword);
-            //subject.Add(X509Name.,szCountry);
+            subject.Add(X509Name.C, _settings.Country);
+            subject.Add(X509Name.ST, _settings.Province);
+            subject.Add(X509Name.L, _settings.City);
+            subject.Add(X509Name.O, _settings.Company);
+            subject.Add(X509Name.OU, _settings.Organization);
+            subject.Add(X509Name.CN, _settings.Common);
+            subject.Add(X509Name.EmailAddress,_settings.Email);
+            subject.ChallangePassword(_settings.ChallengePassword);
 
             var cer = X509Certificate2Builder.GeneratePkcs10(subject);
-            //var ca2 = new X509Certificate2Builder { SubjectName = subject }.Build();
-            //return ca2.Thumbprint;
+
             return cer;
         }
 
         bool API_GetSessionKey()
         {
-            var msg = $"{api_msgformat}~{_apiClientId}~CreateSessionKey~{api_company}~{_apiMachineId}~";
+            var msg = $"{_settings.MessageFormat}~{_settings.ClientId}~CreateSessionKey~{_settings.Company}~{_settings.MachineId}~";
 
-            LOG(DEBUG, $"[API] Requesting Session Key: {msg}");
+            Logger.Debug($"[API] Requesting Session Key: {msg}");
             
             var resp = noConnect ?
                 File.ReadAllBytes("Session.txt")
-                : _sslClient.Send(msg, vcasServerAddress, VCAS_Port_SSL);
-            //var resp = 
+                : _sslClient.Send(msg, _settings.VcasServer, _settings.VcasPort);
 
             if (resp == null) return false;
 
             var encoding = Encoding.ASCII;
 
             var responseBuffer = encoding.GetChars(resp);
-            session_key = new string(responseBuffer, 4, 16);
-            timestamp = new string(responseBuffer, 20, 20);
-            LOG(DEBUG, $"[API] Session key '{session_key}' obtained, timestamp: '{timestamp}' with encoding {encoding.EncodingName}");
+            _sessionKey = new string(responseBuffer, 4, 16);
+            _timestamp = new string(responseBuffer, 20, 20);
+            Logger.Debug($"[API] Session key '{_sessionKey}' obtained, timestamp: '{_timestamp}' with encoding {encoding.EncodingName}");
             
             return true;
         }
 
-        void vm_config(string vcas_address, int vcas_port, string vks_address, int vks_port, string company, int interval, string dir)
+        void load_machineid()
         {
-            vcasServerAddress = vcas_address;
-            vksServerAddress = vks_address;
-
-            api_company = company;
-
-            VCAS_Port_SSL= vcas_port;
-            VKS_Port_SSL = vks_port;
-
-
-        }
-
-        int load_machineid()
-        {
-            _apiMachineId = string.Empty;
+            _settings.MachineId = string.Empty;
             
             if (File.Exists("machineId"))
             {
-                LOG(DEBUG, "[API] MachineID found, reading MachineID");
-                _apiMachineId = File.ReadAllText("machineId");
-                if (_apiMachineId.Length == 28) return 0;
+                Logger.Debug("[API] MachineID found, reading MachineID");
+                _settings.MachineId = File.ReadAllText("machineId");
+                if (_settings.MachineId.Length == 28) return;
             }
-            
-            LOG(DEBUG, "[API] No MachineID found, generating MachineID");
+
+            Logger.Debug("[API] No MachineID found, generating MachineID");
             var buf = new byte[20];
             _random.NextBytes(buf);
-            _apiMachineId = Convert.ToBase64String(buf);
-            LOG(DEBUG, $"[API] Your MachineID is: {_apiMachineId}");
-            File.WriteAllText("machineId",_apiMachineId);
-            return 0;
+            _settings.MachineId = Convert.ToBase64String(buf);
+            Logger.Debug($"[API] Your MachineID is: {_settings.MachineId}");
+            File.WriteAllText("machineId",_settings.MachineId);
         }
 
-        int load_clientId()
+        void load_clientId()
         {
-            _apiClientId = string.Empty;
+            _settings.ClientId = string.Empty;
 
             if (File.Exists("clientId"))
             {
-                LOG(DEBUG, "[API] ClientID found, reading ClientId");
-                _apiClientId = File.ReadAllText("clientId");
-                if (_apiClientId.Length == 56) return 0;
+                Logger.Debug("[API] ClientID found, reading ClientId");
+                _settings.ClientId = File.ReadAllText("clientId");
+                if (_settings.ClientId.Length == 56) return;
             }
 
-            LOG(DEBUG, "[API] No ClientId found, generating ClientId");
+            Logger.Debug("[API] No ClientId found, generating ClientId");
             var buf = new byte[28];
             _random.NextBytes(buf);
-            _apiClientId = string.Empty;
+            _settings.ClientId = string.Empty;
             foreach (var b in buf)
             {
-                _apiClientId += (b & 0xFF).ToString("X2");
+                _settings.ClientId += (b & 0xFF).ToString("X2");
             }
-            //_apiClientId = Convert.ToBase64String(buf);
-            LOG(DEBUG, $"[API] Your ClientID is: {_apiClientId}");
-            File.WriteAllText("clientId", _apiClientId);
-            return 0;
-        }
-
-        private void LOG(int level, string message)
-        {
-            Console.WriteLine(message);
+            Logger.Debug($"[API] Your ClientID is: {_settings.ClientId}");
+            File.WriteAllText("clientId", _settings.ClientId);
         }
     }
 }
