b3c749ad9cfaf94f5c0af4b547345b53b42c159a
jansaris <jan.saris@gmail.com>
2016-03-31 08:43:10 +0200

    Commit jspm packages

---
+++ WebUi/Content/jspm_packages/github/frankwallis/plugin-typescript@2.6.0.js
@@ -0,0 +1,3 @@
+System.register(["github:frankwallis/plugin-typescript@2.6.0/plugin"], function($__export) {
+  return {  setters: [function(m) { for (var p in m) $__export(p, m[p]); }],  execute: function() {}  };
+});
