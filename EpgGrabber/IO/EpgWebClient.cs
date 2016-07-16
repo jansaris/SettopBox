94913263bc2aa3af25fac5dde8a81d9816b9a4ce
Jan Saris <jan.saris@gmail.com>
2016-07-16 21:48:13 +0200

    Added extra logging to EpgWebClient

--- EpgGrabber/IO/EpgWebClient.cs
+++ EpgGrabber/IO/EpgWebClient.cs
@@ -1,21 +1,25 @@
 ﻿using System;
 using System.Net;
+using log4net;
 
 namespace EpgGrabber.IO
 {
     public class EpgWebClient : WebClient
     {
+        readonly ILog _logger;
         readonly int _timeout;
 
-        public EpgWebClient(Settings settings)
+        public EpgWebClient(ILog logger, Settings settings)
         {
+            _logger = logger;
             _timeout = settings.WebRequestTimeoutInMs;
         }
 
         protected override WebRequest GetWebRequest(Uri uri)
         {
+            _logger.Debug($"Generate request for {uri} with {_timeout}ms timeout");
             var w = base.GetWebRequest(uri);
-            w.Timeout = _timeout; //Allow max 5 seconds
+            w.Timeout = _timeout; 
             return w;
         }
     }
