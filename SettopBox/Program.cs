8dbf923b38ae9c4f45ecfde2959ba9d0418cef52
jansaris <jan.saris@gmail.com>
2016-07-22 08:31:41 +0200

    Implemented pid file to check if the application is running

--- SettopBox/Program.cs
+++ SettopBox/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using log4net;
@@ -9,7 +10,11 @@ namespace SettopBox
 {
     class Program
     {
-        static void Main(string[] args)
+        static readonly string PidFile = Path.Combine(AssemblyDirectory, "SettopBox.pid");
+        static readonly ILog Logger = LogManager.GetLogger("SettopBox");
+
+
+        static void Main()
         {
             Directory.SetCurrentDirectory(AssemblyDirectory);
             var container = SharedContainer.CreateAndFill<DependencyConfig,
@@ -19,22 +24,69 @@ namespace SettopBox
                                                           EpgGrabber.DependencyConfig,
                                                           TvHeadendIntegration.DependencyConfig,
                                                           WebUi.DependencyConfig>("Log4net.config");
+
             var settopBox = container.GetInstance<SettopBox>();
             var signal = container.GetInstance<LinuxSignal>();
+            if (CreatePidFile())
+            {
+                Run(settopBox, signal);
+                RemovePidFile();
+            }
+            signal.Dispose();
+            container.Dispose();
+        }
+
+        static bool CreatePidFile()
+        {
+            try
+            {
+                var file = new FileInfo(PidFile);
+                if (!file.Exists)
+                {
+                    File.WriteAllText(file.FullName, $"{Process.GetCurrentProcess().Id}");
+                    Logger.Info($"Created pid file {PidFile}");
+                    return true;
+                }
+                var pid = File.ReadAllText(file.FullName);
+                Logger.Warn($"SettopBox is already running under PID {pid} based on {file.FullName}");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error($"Failed to create or validate pid file {PidFile}: {ex.Message}");
+            }
+            return false;
+        }
+
+        static void RemovePidFile()
+        {
+            try
+            {
+                var file = new FileInfo(PidFile);
+                if (!file.Exists) return;
+
+                file.Delete();
+                Logger.Info($"Removed pid file {PidFile}");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error($"Failed to remove the pid file {PidFile}: {ex.Message}");
+            }
+        }
+
+        static void Run(SettopBox settopBox, LinuxSignal signal)
+        {
             settopBox.Start();
             if (Console.IsInputRedirected)
             {
-                LogManager.GetLogger("SettopBox").Info("Wait for kill-signal");
+                Logger.Info("Wait for kill-signal");
                 signal.WaitForListenThreadToComplete();
             }
             else
             {
-                LogManager.GetLogger("SettopBox").Info("Wait for keyboard input");
+                Logger.Info("Wait for keyboard input");
                 Console.WriteLine("Press enter to exit");
                 Console.ReadLine();
             }
-            signal.Dispose();
-            container.Dispose();
         }
 
         static string AssemblyDirectory
