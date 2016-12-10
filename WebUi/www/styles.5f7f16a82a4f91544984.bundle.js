582d619a3c7919ed62ed826b9846f2216c447e14
Jan Saris <jan.saris@gmail.com>
2016-12-10 15:54:39 +0100

    Builded ui for release

--- WebUi/www/styles.5f7f16a82a4f91544984.bundle.js
+++ WebUi/www/styles.5f7f16a82a4f91544984.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([2,3],{387:function(n,o){},388:function(n,o){},389:function(n,o){},689:function(n,o,c){c(387),c(388),n.exports=c(389)}},[689]);
+webpackJsonp([2,3],{388:function(n,o){},389:function(n,o){},390:function(n,o){},697:function(n,o,c){c(388),c(389),n.exports=c(390)}},[697]);
 //# sourceMappingURL=styles.5f7f16a82a4f91544984.bundle.map
