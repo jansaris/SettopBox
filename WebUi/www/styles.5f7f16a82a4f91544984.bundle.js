bee696de64b3098e886c0db30fd64ce5286c945f
Jan Saris <jan.saris@gmail.com>
2016-12-18 15:52:28 +0100

    Updated release version of webui

--- WebUi/www/styles.5f7f16a82a4f91544984.bundle.js
+++ WebUi/www/styles.5f7f16a82a4f91544984.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([2,3],{387:function(n,o){},388:function(n,o){},389:function(n,o){},697:function(n,o,c){c(387),c(388),n.exports=c(389)}},[697]);
+webpackJsonp([2,3],{388:function(n,o){},389:function(n,o){},390:function(n,o){},701:function(n,o,c){c(388),c(389),n.exports=c(390)}},[701]);
 //# sourceMappingURL=styles.5f7f16a82a4f91544984.bundle.map
