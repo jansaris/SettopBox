669d95bd29f6728505337db98a9f03c5eca563bb
Jan Saris <jan.saris@gmail.com>
2017-01-01 22:46:32 +0100

    Updated released version

--- WebUi/www/styles.5f7f16a82a4f91544984.bundle.js
+++ WebUi/www/styles.5f7f16a82a4f91544984.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([2,3],{388:function(n,o){},389:function(n,o){},390:function(n,o){},701:function(n,o,c){c(388),c(389),n.exports=c(390)}},[701]);
+webpackJsonp([2,3],{389:function(n,o){},390:function(n,o){},391:function(n,o){},704:function(n,o,c){c(389),c(390),n.exports=c(391)}},[704]);
 //# sourceMappingURL=styles.5f7f16a82a4f91544984.bundle.map
