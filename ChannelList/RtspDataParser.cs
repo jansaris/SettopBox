58693c7021f99faaf95ba1659c69fabf5e84e258
Jan Saris <jan.saris@gmail.com>
2017-10-16 22:29:17 +0200

    Added first part of Rtsp channel reader

---
+++ ChannelList/RtspDataParser.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharedComponents.Models;
+
+namespace ChannelList
+{
+    public class RtspDataParser
+    {
+        public List<ChannelInfo> ParseChannels(byte[] rtspData)
+        {
+            var data = Encoding.ASCII.GetString(rtspData).Replace("\r", "").Split('\n');
+            var channels = new List<ChannelInfo>();
+
+            var channelStart = Array.FindIndex(data, 0, s => s == string.Empty);
+            while (channelStart > 0)
+            {
+                var channelEnd = Array.FindIndex(data, channelStart, s => s.StartsWith("--"));
+                if (channelEnd == -1) channelEnd = data.Length;
+                channels.Add(ParseChannel(data.Skip(channelStart).Take(channelEnd - channelStart)));
+                channelStart = channelEnd != data.Length ? channelEnd + 1 : -1;
+            }
+
+            return channels;
+        }
+
+        private ChannelInfo ParseChannel(IEnumerable<string> messageBlock)
+        {
+            var channel = new ChannelInfo();
+            foreach (var line in messageBlock)
+            {
+                if(line.Length == 0) continue;
+                
+                switch (line[0])
+                {
+                    case 's': channel.Name = ExtractChannelName(line);
+                        channel.Number = ExtractChannelNumber(line);
+                        break;
+                }
+            }
+            return channel;
+        }
+
+        private int ExtractChannelNumber(string line)
+        {
+            var end = line.IndexOf(" ", StringComparison.Ordinal);
+            return int.Parse(line.Substring(2, end - 2));
+        }
+
+        private string ExtractChannelName(string line)
+        {
+            var start = line.IndexOf(" ", StringComparison.Ordinal);
+            return line.Substring(start + 1);
+        }
+    }
+}
---
+++ ChannelList/RtspDataReceiver.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChannelList
+{
+    public class RtspDataReceiver
+    {
+        public byte[] ReadDataFromServer(string host, int port)
+        {
+            var sock = new Socket(SocketType.Stream, ProtocolType.Tcp) {ReceiveTimeout = 500};
+            sock.Connect(host, port);
+            sock.Send(Encoding.ASCII.GetBytes($"DESCRIBE rtsp://{host}/vqe-channels/ RTSP/1.0\r\nCSeq: 1\r\nAccept: application/sdp\r\n\r\n"));
+            var buffer = new byte[1024];
+            var data = new List<byte>();
+            while (true)
+            {
+                try
+                {
+                    var receivedData = sock.Receive(buffer);
+                    if (receivedData > 0) data.AddRange(buffer.Take(receivedData));
+                    if (receivedData == 0) break;
+                }
+                catch (SocketException)
+                {
+                    //We got no data for 500ms, so we are at the end of the stream
+                    break;
+                }
+                
+            }
+
+            return data.ToArray();
+        }
+    }
+}
