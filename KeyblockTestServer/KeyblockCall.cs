d3b9c052019a6164a0147ab7ffbbc843c1d79c70
Jan Saris <jan.saris@gmail.com>
2017-10-21 15:04:31 +0200

    Made keyblocktestserver independentant from keyblock

--- KeyblockTestServer/KeyblockCall.cs
+++ KeyblockTestServer/KeyblockCall.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using Keyblock;
 using log4net;
 
 namespace KeyblockTestServer
---
+++ KeyblockTestServer/RC4.cs
@@ -0,0 +1,46 @@
+﻿namespace KeyblockTestServer
+{
+    // ReSharper disable once InconsistentNaming
+    public static class RC4
+    {
+        public static byte[] Encrypt(byte[] pwd, byte[] data)
+        {
+            int a, i, j, tmp;
+
+            var key = new int[256];
+            var box = new int[256];
+            var cipher = new byte[data.Length];
+
+            for (i = 0; i < 256; i++)
+            {
+                key[i] = pwd[i % pwd.Length];
+                box[i] = i;
+            }
+            for (j = i = 0; i < 256; i++)
+            {
+                j = (j + box[i] + key[i]) % 256;
+                tmp = box[i];
+                box[i] = box[j];
+                box[j] = tmp;
+            }
+            for (a = j = i = 0; i < data.Length; i++)
+            {
+                a++;
+                a %= 256;
+                j += box[a];
+                j %= 256;
+                tmp = box[a];
+                box[a] = box[j];
+                box[j] = tmp;
+                var k = box[((box[a] + box[j]) % 256)];
+                cipher[i] = (byte)(data[i] ^ k);
+            }
+            return cipher;
+        }
+
+        public static byte[] Decrypt(byte[] pwd, byte[] data)
+        {
+            return Encrypt(pwd, data);
+        }
+    }
+}
